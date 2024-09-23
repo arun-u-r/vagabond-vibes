c6f6b048462228f18fbce44abed708add30080c7
arun-u-r <arununithroth@gmail.com>
2024-09-23 11:08:25 +0530

    updated image paths

--- src/assets/data/gallery.js
+++ src/assets/data/gallery.js
@@ -3,37 +3,37 @@ const gallery = {
   images: [
     {
       id: 1,
-      src: 'pexels-ryutaro-tsukata-5472304.jpg',
+      src: `${import.meta.env.BASE_URL}pexels-ryutaro-tsukata-5472304.jpg`,
       label: "Tranquil Escapes",
       description: "Unwind in serene locations, where time slows down and the soul finds peace"
     },
     {
       id: 2,
-      src: '/plane.jpg',
+      src: `${import.meta.env.BASE_URL}plane.jpg`,
       label: "Festival Fever",
       description: "Feel the vibrant energy of unique festivals that celebrate freedom, music, and life"
     },
     {
       id: 3,
-      src: '/pexels-kyle-miller-15578965.jpg',
+      src: `${import.meta.env.BASE_URL}pexels-kyle-miller-15578965.jpg`,
       label: "Eco-Friendly Stays",
       description: "Sustainably crafted accommodations that connect you with nature and leave a minimal footprint"
     },
     {
       id: 4,
-      src: '/plane.jpg',
+      src: `${import.meta.env.BASE_URL}plane.jpg`,
       label: "Cultural Immersions",
       description: "Dive deep into local cultures, traditions, and arts for a truly authentic journey."
     },
     {
       id: 5,
-      src: '/pexels-ryutaro-tsukata-5472304.jpg',
+      src: `${import.meta.env.BASE_URL}pexels-ryutaro-tsukata-5472304.jpg`,
       label: "Nomadic Lifestyle",
       description: "Embrace the spirit of wandering and live a life untethered, full of exploration and spontaneity."
     },
     {
       id: 6,
-      src: '/space-wallpapers.jpg',
+      src: `${import.meta.env.BASE_URL}space-wallpapers.jpg`,
       label: "Soulful Journeys",
       description: "Embark on meaningful adventures that inspire growth, self-discovery, and lasting connections"
     },
--- src/components/HeroBlock.jsx
+++ src/components/HeroBlock.jsx
@@ -3,13 +3,13 @@ import React from "react"
 const HeroBlock = () => {
     return (
         <div className="w-full h-[90vh] ">
-            <img src="/wp13100987-vagabond-pc-wallpapers.png" alt="v-v-favicon-black.svg"
+            <img src={`${import.meta.env.BASE_URL}wp13100987-vagabond-pc-wallpapers.png`} alt="v-v-favicon-black.svg"
                 className="w-full h-full object-cover"
             />
             <div className="max-w-[1140px] m-auto">
                 <div className="absolute top-[10%] w-full md:-[50%] max-w-[500px] h-full flex flex-col text-white p-4 ">
-                    <img src="v-v-logo-white.svg" alt="" className=" ml-10 p-2 w-[9rem] h-[9rem] object-cover " />
-                    <h1 className="text-2xl md:text-4xl lg:text-2xl mt-4 ">find Somthing that u lost</h1>
+                    <img src={`${import.meta.env.BASE_URL}v-v-logo-white.svg`} alt="" className=" ml-10 p-2 w-[9rem] h-[9rem] object-cover " />
+                    <h1 className="text-2xl md:text-4xl lg:text-2xl ">find Somthing that u lost</h1>
                     <h2 className="text-xl md:text-2xl lg:text-4xl mb-5">With Vagabond vibes</h2>
                     <p style={{ color: '#F6D448' }} className="text-sm md:text-base lg:text-lg">
                     &quot;Discover a world of freedom, adventure, and connection with Vagabond Vibes. 
